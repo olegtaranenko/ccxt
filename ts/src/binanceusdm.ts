0e00bcdf5237b126b91983fcb98d543a88595d89
Oleg Taranenko MP <olegtaranenko@gmail.com>
2024-02-11 03:32:49 +0100

    sorting other binance clones

--- ts/src/binanceusdm.ts
+++ ts/src/binanceusdm.ts
@@ -9,33 +9,6 @@ import { InvalidOrder } from './base/errors.js';
 export default class binanceusdm extends binance {
     describe () {
         return this.deepExtend (super.describe (), {
-            'id': 'binanceusdm',
-            'name': 'Binance USDⓈ-M',
-            'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
-                'doc': [
-                    'https://binance-docs.github.io/apidocs/futures/en/',
-                    'https://binance-docs.github.io/apidocs/spot/en',
-                ],
-            },
-            'has': {
-                'CORS': undefined,
-                'spot': false,
-                'margin': false,
-                'swap': true,
-                'future': true,
-                'option': undefined,
-                'createStopMarketOrder': true,
-            },
-            'options': {
-                'fetchMarkets': [ 'linear' ],
-                'defaultSubType': 'linear',
-                // https://www.binance.com/en/support/faq/360033162192
-                // tier amount, maintenance margin, initial margin
-                'leverageBrackets': undefined,
-                'marginTypes': {},
-                'marginModes': {},
-            },
             // https://binance-docs.github.io/apidocs/futures/en/#error-codes
             'exceptions': {
                 'exact': {
@@ -43,6 +16,33 @@ export default class binanceusdm extends binance {
                     '-5022': InvalidOrder, // {"code":-5022,"msg":"Due to the order could not be executed as maker, the Post Only order will be rejected."}
                     '-5028': InvalidOrder, // {"code":-5028,"msg":"Timestamp for this request is outside of the ME recvWindow."}
                 },
+            },
+            'has': {
+                'CORS': undefined,
+                'createStopMarketOrder': true,
+                'future': true,
+                'margin': false,
+                'option': undefined,
+                'spot': false,
+                'swap': true,
+            },
+            'id': 'binanceusdm',
+            'name': 'Binance USDⓈ-M',
+            'options': {
+                'defaultSubType': 'linear',
+                'fetchMarkets': [ 'linear' ],
+                // https://www.binance.com/en/support/faq/360033162192
+                // tier amount, maintenance margin, initial margin
+                'leverageBrackets': undefined,
+                'marginModes': {},
+                'marginTypes': {},
+            },
+            'urls': {
+                'doc': [
+                    'https://binance-docs.github.io/apidocs/futures/en/',
+                    'https://binance-docs.github.io/apidocs/spot/en',
+                ],
+                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
             },
         });
     }
--- ts/src/pro/binanceusdm.ts
+++ ts/src/pro/binanceusdm.ts
@@ -9,15 +9,6 @@ import { InvalidOrder } from '../base/errors.js';
 export default class binanceusdm extends binance {
     describe () {
         return this.deepExtend (super.describe (), {
-            'id': 'binanceusdm',
-            'name': 'Binance USDⓈ-M',
-            'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
-            },
-            'options': {
-                'fetchMarkets': [ 'linear' ],
-                'defaultSubType': 'linear',
-            },
             // https://binance-docs.github.io/apidocs/futures/en/#error-codes
             'exceptions': {
                 'exact': {
@@ -25,6 +16,15 @@ export default class binanceusdm extends binance {
                     '-5022': InvalidOrder, // {"code":-5022,"msg":"Due to the order could not be executed as maker, the Post Only order will be rejected."}
                     '-5028': InvalidOrder, // {"code":-5028,"msg":"Timestamp for this request is outside of the ME recvWindow."}
                 },
+            },
+            'id': 'binanceusdm',
+            'name': 'Binance USDⓈ-M',
+            'options': {
+                'defaultSubType': 'linear',
+                'fetchMarkets': [ 'linear' ],
+            },
+            'urls': {
+                'logo': 'https://user-images.githubusercontent.com/1294454/117738721-668c8d80-b205-11eb-8c49-3fad84c4a07f.jpg',
             },
         });
     }
