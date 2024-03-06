c67fcbfe46f42ea1968a37ca21adf49661ec24d3
Oleg Taranenko MP <olegtaranenko@gmail.com>
2024-03-07 00:09:56 +0100

    wrong merge

--- ts/src/pro/binance.ts
+++ ts/src/pro/binance.ts
@@ -86,8 +86,8 @@ export default class binance extends binanceRest {
                 // or every 0ms in real-time for futures
                 'watchOrderBookRate': 100,
                 'watchPositions': {
-                    'fetchPositionsSnapshot': true, // or false
                     'awaitPositionsSnapshot': true, // whether to wait for the positions snapshot before providing updates
+                    'fetchPositionsSnapshot': true, // or false
                 },
                 'watchTicker': {
                     'name': 'ticker', // ticker = 1000ms L1+OHLCV, bookTicker = real-time L1
@@ -2634,7 +2634,7 @@ export default class binance extends binanceRest {
         return this.safePosition ({
             'collateral': undefined,
             'contractSize': undefined,
-            'contracts': parseNumber (contractsAbs),
+            'contracts': this.parseNumber (contractsAbs),
             'datetime': undefined,
             'entryPrice': this.safeNumber (position, 'ep'),
             'hedged': hedged,
@@ -2782,7 +2782,6 @@ export default class binance extends binanceRest {
         //
         //    {
         //        "id": "f4ce6a53-a29d-4f70-823b-4ab59391d6e8",
-        //        "status": 200,
         //        "result": [
         //            {
         //                "id": 0,
@@ -2795,6 +2794,7 @@ export default class binance extends binanceRest {
         //            }
         //            ...
         //        ],
+        //        "status": 200,
         //    }
         //
         //
@@ -3028,7 +3028,6 @@ export default class binance extends binanceRest {
             '24hrTicker': this.handleTicker,
             '24hrTicker@arr': this.handleTickers,
             'ACCOUNT_UPDATE': this.handleAcountUpdate,
-            'ORDER_TRADE_UPDATE': this.handleOrderUpdate,
             'aggTrade': this.handleTrade,
             'balanceUpdate': this.handleBalance,
             'bookTicker': this.handleTicker,
@@ -3037,6 +3036,7 @@ export default class binance extends binanceRest {
             'indexPrice_kline': this.handleOHLCV,
             'kline': this.handleOHLCV,
             'markPrice_kline': this.handleOHLCV,
+            'ORDER_TRADE_UPDATE': this.handleOrderUpdate,
             'outboundAccountPosition': this.handleBalance,
             'trade': this.handleTrade,
         };
